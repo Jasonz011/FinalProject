61454cc0823f569c725601f79bd2b955d584cda5
BT_2N2_14 <sample@bths.edu>
2024-05-20 13:48:53 -0400

    Created an outline for all the classes

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,30 @@
-public class GameFrame {
-    public GameFrame() {
+import javax.swing.*;
 
+public class GameFrame implements Runnable {
+
+    private GamePanel panel;
+
+    public GameFrame(String name) {
+        JFrame frame = new JFrame("Word Hunt");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(960, 580); // 540 height of image + 40 for window menu bar
+        frame.setLocationRelativeTo(null); // auto-centers frame in screen
+
+        // create and add panel
+        panel = new GamePanel(name);
+        frame.add(panel);
+
+        // display the frame
+        frame.setVisible(true);
+
+        // start thread, required for animation
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        while (true) {
+            panel.repaint();  // we don't ever call "paintComponent" directly, but call this to refresh the panel
+        }
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,76 @@
-public class GamePanel {
-    public GamePanel() {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class GamePanel extends JPanel implements KeyListener, MouseListener, ActionListener {
+    private BufferedImage background;
+
+    private boolean[] pressedKeys;
+
+    private Timer timer;
+    private int time;
+
+    public GamePanel(String name) {
+        try {
+            background = ImageIO.read(new File(""));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        time = 0;
+        timer = new Timer(1000, this); // this Timer will call the actionPerformed interface method every 1000ms = 1 second
+        timer.start();
+        addKeyListener(this);
+        addMouseListener(this);
+        setFocusable(true); // this line of code + one below makes this panel active for keylistener events
+        requestFocusInWindow(); // see comment above
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);  // just do this
+        g.drawImage(background, 0, 0, null);
+        g.setFont(new Font("Courier New", Font.BOLD, 24));
+        g.drawString("Time: " + time, 20, 70);
+    }
+
+    // ----- KeyListener interface methods -----
+    public void keyTyped(KeyEvent e) { } // unimplemented
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+    }
+
+    // ----- MouseListener interface methods -----
+    public void mouseClicked(MouseEvent e) { }
+    // this method isn't called, so mouseReleased is best
+
+    public void mousePressed(MouseEvent e) { } // unimplemented
+
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {  // left mouse click
+
+        } else {
+            Point mouseClickLocation = e.getPoint();
+        }
+    }
+
+    public void mouseEntered(MouseEvent e) { } // unimplemented
+
+    public void mouseExited(MouseEvent e) { } // unimplemented
+
+    // ACTIONLISTENER INTERFACE METHODS: used for buttons AND timers!
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof Timer) {
+            time++;
+        }
     }
 }
