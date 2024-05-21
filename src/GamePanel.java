7780cac8c1e6e3a24ba1b7129cf41fa74a5d7231
BT_2N2_15 <sample@bths.edu>
2024-05-21 14:01:09 -0400

    added some logic and a dictionary file

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,28 +6,45 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GamePanel extends JPanel implements KeyListener, MouseListener, ActionListener {
     private BufferedImage background;
 
     private boolean[] pressedKeys;
 
+    private ArrayList<String> words;
+
     private Timer timer;
     private int time;
 
     public GamePanel(String name) {
+        readData();
         try {
             background = ImageIO.read(new File(""));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        time = 0;
+        time = 60;
         timer = new Timer(1000, this); // this Timer will call the actionPerformed interface method every 1000ms = 1 second
         timer.start();
         addKeyListener(this);
         addMouseListener(this);
         setFocusable(true); // this line of code + one below makes this panel active for keylistener events
         requestFocusInWindow(); // see comment above
+    }
+    public void readData() {
+        words = new ArrayList<>();
+        try {
+            File myFile = new File("src\\words.txt");
+            Scanner fileScanner = new Scanner(myFile);
+            while (fileScanner.hasNext()) {
+                String word = fileScanner.nextLine();
+                words.add(word);
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
     @Override
@@ -70,7 +87,7 @@ public class GamePanel extends JPanel implements KeyListener, MouseListener, Act
     // ACTIONLISTENER INTERFACE METHODS: used for buttons AND timers!
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof Timer) {
-            time++;
+            time--;
         }
     }
 }
